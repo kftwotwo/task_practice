253b5472ed5736c726eeeef176aed816d1a7738a
Kevin Finley <Kevin@Kevins-MBP.lan>
2016-11-27 19:42:37 -0800

    add third spec and the 2nd one will pass

--- spec/task_spec.rb
+++ spec/task_spec.rb
@@ -15,3 +15,11 @@ describe(".all") do
     expect(Task.all()).to(eq([]))
   end
 end
+
+describe("#save") do
+  it("adds a task to the array of saved tasks") do
+    test_task = Task.new("wash the lion")
+    test_task.save()
+    expect(Task.all()).to(eq([test_task]))
+  end
+end
